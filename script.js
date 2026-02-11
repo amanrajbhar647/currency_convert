cee9aec1e9248f591665965f0ec01d35b4121f81
Aman Rajbhar <aman.22scse1012617@galgotiasuniversity.edu.in>
2026-02-11 17:38:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+ const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/usd/inr.json";
+
+ const dropdown= document.querySelectorAll(".dropdown select");
+  const btn = document.querySelector("form button");
+  const fromCurr=document.querySelector(".from select");
+  const toCurr=document.querySelector(".to select");;
+  const msg=document.querySelector(".msg");
+
+
+//  for (code in countryList){
+//     console.log(code,countryList[code])
+//  }
+for(let select of dropdown){
+    for(currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode
+        newOption.value=currCode;
+        if(select.name === "from"  && currCode === "USD"){
+            newOption.selected ="selected";
+
+        } else  if(select.name === "to"  && currCode === "INR"){
+            newOption.selected ="selected";
+
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+
+
+    });
+}
+//IF I CHANGE the country code then become to change with flag
+//new fuction creat the updateFlag
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+
+ btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+
+    let response = await fetch(URL);
+    let data = await response.json();   // ✅ FIX
+
+    let rate = data[toCurr.value.toLowerCase()]; // ✅ Get rate
+
+    let finalAmt = amtVal * rate;
+
+    document.querySelector(".msg").innerText =
+        `${amtVal} ${fromCurr.value} = ${finalAmt.toFixed(2)} ${toCurr.value}`;
+});
+
